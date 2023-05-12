1a9f23eec87185abe75938fdfbc90a72ab39c0c3
André Coêlho <alcbcoelho@gmail.com>
2023-05-11 22:06:59 -0300

    Implementação de validadores/estado de mudanças no formulário de login usando a biblioteca React Hook Form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,38 +1,32 @@
-import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";    // hook useForm, ideal para trabalhar com formulários (ao invés de usar useRef, como foi feito no commit anterior a esse; COMPARAR OS DOIS COMMITS E VER O QUE MUDA)
 
 const Login = (props) => {
   const navigate = useNavigate(); // useNavigate: hook que gera um objeto (navigate) que é posteriormente chamado como uma função
-  const refInputEmail = useRef();   // useRef: hook p/ referenciar elemento HTML/componente cuja propriedade "ref" corresponda à variável;
-  const refInputSenha = useRef();
-  const [ errorEmail, setErrorEmail ] = useState();
-
-  function handleBlurInputEmail(event) {
-    if (refInputEmail.current.value.length === 0) {
-      refInputEmail.current.focus()
-      setErrorEmail("Email é obrigatório")
-     } else setErrorEmail(null);
-  }
-
-  function handleSubmit(event) {  // evento 'submit' é disparado mesmo se o botão dentro das tags <form> não for tecnicamente um input com type="submit"
-    event.preventDefault();   // cancela o refresh que por padrão ocorre durante um evento 'submit'
-    handleBlurInputEmail(event);
-  }
-
+  const form = useForm();
+  const { register, handleSubmit, formState: { errors } } = form;  // o objeto 'register' guarda os atributos "name", "ref" de um elemento input.
+  
   function handleClick(event) {
     props.onLogin(event);
     navigate("/"); // faz com que a rota atual mude para a rota principal
   }
 
+  function handleFormSubmit(data) {
+    console.log(data)
+  }
+
   return (
     <>
       <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
         <label htmlFor="email">E-mail: </label>
-        <input type="email" id="email" name="email" ref={refInputEmail}/>
-        {errorEmail && <p>{errorEmail}</p>}
+        <input type="email" id="email" {...register("email", /* Validadores (melhor que fazer ternários) */ {
+          required: { value: true, message: "Email obrigatório" },
+          pattern: { value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g, message: "Email inválido"}
+        }) /* = name="email" ref="{}" */} />
+        {errors.email && <p>{errors.email.message}</p>  /* se tem erro no campo 'email', renderizar a respectiva mensagem de erro ("Email obrigatório" ou "email inválido")*/}
         <label htmlFor="senha">Senha: </label>
-        <input type="password" id="senha" name="senha" ref={refInputSenha} />
+        <input type="password" id="senha" {...register("senha")}/>
         <button>Entrar</button>
       </form>
     </>
@@ -40,3 +34,5 @@ const Login = (props) => {
 };
 
 export default Login;
+
+// Para mais informações, checar: https://react-hook-form.com/
